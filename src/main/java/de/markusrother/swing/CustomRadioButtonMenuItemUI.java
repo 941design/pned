040ef1755a91a0108b9d52a81b2ed59efb80286e
Markus Rother <informatik@markusrother.de>
2015-01-02 23:07:32 +0100

    + Factored CustomRadioButtonMenuItemUI out of CustomRadioButtonMenuItem

---
+++ src/main/java/de/markusrother/swing/CustomRadioButtonMenuItemUI.java
@@ -0,0 +1,90 @@
+package de.markusrother.swing;
+
+import java.awt.Rectangle;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.MenuSelectionManager;
+import javax.swing.plaf.basic.BasicRadioButtonMenuItemUI;
+
+import sun.swing.MenuItemLayoutHelper;
+
+public class CustomRadioButtonMenuItemUI extends BasicRadioButtonMenuItemUI
+	implements
+		MouseListener {
+
+	private MenuSelectionManager cachedMsm;
+
+	CustomRadioButtonMenuItemUI(final CustomRadioButtonMenuItem menuItem) {
+		this.menuItem = menuItem;
+	}
+
+	private void waitForCachingMsm() {
+		while (cachedMsm == null) {
+			try {
+				wait();
+			} catch (final InterruptedException e1) {
+				// IGNORE
+			}
+		}
+	}
+
+	@Override
+	protected synchronized void doClick(final MenuSelectionManager msm) {
+		// We will proceed upon determining whether or not the MouseEvent
+		// was on the radio button. Caching is necessary if we do not rely
+		// on the order of registered MouseListeners.
+		this.cachedMsm = msm;
+		notify();
+	}
+
+	@Override
+	public synchronized void mouseClicked(final MouseEvent e) {
+		final Rectangle textRect = getTextRect();
+		// We might as well listen for the (small) checkRect
+		if (textRect.contains(e.getPoint())) {
+			waitForCachingMsm();
+			super.doClick(cachedMsm);
+		} else {
+			// FIXME - create and call custom event listener
+			// Only radio button was toggled!
+			menuItem.setSelected(true);
+		}
+		cachedMsm = null;
+	}
+
+	private Rectangle getTextRect() {
+		// This code was copied from
+		// javax.swing.plaf.basic.BasicMenuItemUI.paintMenuItem()
+		// Not sure if there is a cleaner way how to retrieve the actual
+		// rect.
+		final Rectangle viewRect = new Rectangle(0, 0, menuItem.getWidth(), menuItem.getHeight());
+		final MenuItemLayoutHelper lh = new MenuItemLayoutHelper(menuItem, checkIcon, arrowIcon, viewRect,
+				defaultTextIconGap, acceleratorDelimiter, true, menuItem.getFont(), acceleratorFont,
+				MenuItemLayoutHelper.useCheckAndArrow(menuItem), getPropertyPrefix());
+		final MenuItemLayoutHelper.LayoutResult lr = lh.layoutMenuItem();
+		// lr.getCheckRect();
+		return lr.getTextRect();
+	}
+
+	@Override
+	public void mousePressed(final MouseEvent e) {
+		// IGNORE
+	}
+
+	@Override
+	public void mouseReleased(final MouseEvent e) {
+		// IGNORE
+	}
+
+	@Override
+	public void mouseEntered(final MouseEvent e) {
+		// IGNORE
+	}
+
+	@Override
+	public void mouseExited(final MouseEvent e) {
+		// IGNORE
+	}
+
+}
