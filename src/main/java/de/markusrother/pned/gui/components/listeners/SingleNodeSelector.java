46d58f31d723adcb69d094345b9fcfb7055cf104
mrother <markus.rother@qudosoft.de>
2015-10-30 19:12:37 +0100

    implemented multi selection with CTRL held down

--- src/main/java/de/markusrother/pned/gui/components/listeners/SingleNodeSelector.java
+++ src/main/java/de/markusrother/pned/gui/components/listeners/SingleNodeSelector.java
@@ -1,17 +1,20 @@
 package de.markusrother.pned.gui.components.listeners;
 
-import static de.markusrother.pned.gui.control.events.NodeMultiSelectionEvent.Type.CANCEL;
-import static de.markusrother.pned.gui.control.events.NodeMultiSelectionEvent.Type.SELECT;
-
-import java.awt.Component;
-import java.awt.Point;
-import java.awt.event.MouseEvent;
-import java.util.Arrays;
-
 import de.markusrother.pned.gui.components.AbstractNodeComponent;
 import de.markusrother.pned.gui.control.events.NodeMultiSelectionEvent;
 import de.markusrother.pned.gui.control.events.PnEventTarget;
 import de.markusrother.swing.DragDropAdapter;
+
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedList;
+
+import static de.markusrother.pned.gui.control.events.NodeMultiSelectionEvent.Type.CANCEL;
+import static de.markusrother.pned.gui.control.events.NodeMultiSelectionEvent.Type.DESELECT;
+import static de.markusrother.pned.gui.control.events.NodeMultiSelectionEvent.Type.FINISH;
+import static de.markusrother.pned.gui.control.events.NodeMultiSelectionEvent.Type.SELECT;
 
 /**
  * Creates selection by clicking on a single component.
@@ -19,19 +22,20 @@ import de.markusrother.swing.DragDropAdapter;
  * @author Markus Rother
  * @version 1.0
  */
-public class SingleNodeSelector extends DragDropAdapter<AbstractNodeComponent> {
+public class SingleNodeSelector
+        extends DragDropAdapter<AbstractNodeComponent> {
 
     private final PnEventTarget eventTarget;
+    private final Collection<AbstractNodeComponent> nodes = new LinkedList<>();
 
     /**
      * <p>
      * Constructor for SingleNodeSelector.
      * </p>
      *
-     * @param eventTarget
-     *            a
-     *            {@link de.markusrother.pned.gui.control.events.PnEventTarget}
-     *            object.
+     * @param eventTarget a
+     *                    {@link de.markusrother.pned.gui.control.events.PnEventTarget}
+     *                    object.
      */
     public SingleNodeSelector(final PnEventTarget eventTarget) {
         super(AbstractNodeComponent.class);
@@ -43,11 +47,10 @@ public class SingleNodeSelector extends DragDropAdapter<AbstractNodeComponent> {
      * expectAbstractNode.
      * </p>
      *
-     * @param component
-     *            a {@link java.awt.Component} object.
+     * @param component a {@link java.awt.Component} object.
      * @return a
-     *         {@link de.markusrother.pned.gui.components.AbstractNodeComponent}
-     *         object.
+     * {@link de.markusrother.pned.gui.components.AbstractNodeComponent}
+     * object.
      */
     private AbstractNodeComponent expectAbstractNode(final Component component) {
         try {
@@ -63,10 +66,9 @@ public class SingleNodeSelector extends DragDropAdapter<AbstractNodeComponent> {
      * makeCurrentSelection.
      * </p>
      *
-     * @param node
-     *            a
-     *            {@link de.markusrother.pned.gui.components.AbstractNodeComponent}
-     *            object.
+     * @param node a
+     *             {@link de.markusrother.pned.gui.components.AbstractNodeComponent}
+     *             object.
      */
     private void makeCurrentSelection(final AbstractNodeComponent node) {
         if (node.isSelected() && !node.isPartOfMultiselection()) {
@@ -74,8 +76,11 @@ public class SingleNodeSelector extends DragDropAdapter<AbstractNodeComponent> {
         } else {
             // NOTE - Do NOT rely on event status change within this method!
             // We have to cancel other singly selected nodes, too:
+            nodes.clear();
+            nodes.add(node);
             eventTarget.nodeSelectionCancelled(new NodeMultiSelectionEvent(CANCEL, this));
             eventTarget.nodesSelected(new NodeMultiSelectionEvent(SELECT, this, Arrays.asList(node)));
+            eventTarget.nodeSelectionFinished(new NodeMultiSelectionEvent(FINISH, this, Arrays.asList(node)));
         }
     }
 
@@ -83,7 +88,20 @@ public class SingleNodeSelector extends DragDropAdapter<AbstractNodeComponent> {
     @Override
     public void mouseClicked(final MouseEvent e) {
         super.mouseClicked(e);
-        makeCurrentSelection(expectAbstractNode(e.getComponent()));
+        final AbstractNodeComponent node = expectAbstractNode(e.getComponent());
+        if (e.isControlDown()) {
+            if (node.isSelected()) {
+                nodes.remove(node);
+                eventTarget.nodesSelected(new NodeMultiSelectionEvent(DESELECT, this, Arrays.asList(node)));
+                eventTarget.nodeSelectionFinished(new NodeMultiSelectionEvent(FINISH, this, nodes));
+            } else {
+                nodes.add(node);
+                eventTarget.nodesSelected(new NodeMultiSelectionEvent(SELECT, this, Arrays.asList(node)));
+                eventTarget.nodeSelectionFinished(new NodeMultiSelectionEvent(FINISH, this, nodes));
+            }
+            return;
+        }
+        makeCurrentSelection(node);
     }
 
     /** {@inheritDoc} */
